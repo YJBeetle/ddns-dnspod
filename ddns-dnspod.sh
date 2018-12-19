3f5494c335de5cb9ec10b77d370ecf8dbfdacfb0
YJBeetle <yjbeetle@gmail.com>
2018-12-20 04:10:23 +0800

    ipv6 支持

--- ddns-dnspod.sh
+++ ddns-dnspod.sh
@@ -56,6 +56,26 @@ get_localip()
     return -1
 }
 
+get_localip_curl_v6()
+{
+    ip=$(curl "$1" 2>/dev/null)
+    ip=$(echo "$ip" | grep -oE "((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?")
+    if [ "$ip" = '' ]; then
+        return -1
+    fi
+    echo "$ip"
+}
+
+get_localip_v6()
+{
+    get_localip_curl_v6 'http://ipv6.vm0.test-ipv6.com/ip/' ||
+    get_localip_curl_v6 'http://ipv6.test-ipv6.ke.liquidtelecom.net/ip/' ||
+    get_localip_curl_v6 'http://ipv6.test-ipv6.arauc.br/ip/' ||
+    get_localip_curl_v6 'http://ipv6.test-ipv6.cl/ip/' ||
+    get_localip_curl_v6 'http://ipv6.test-ipv6.vyncke.org/ip/' ||
+    return -1
+}
+
 
 get_domain_id()
 {
@@ -107,12 +127,13 @@ get_record_id()
     login_token=$1
     domain_id=$2
     record=$3
+    record_type=$4
 
     local DOMLVL=0 #初始化节点
     id=''
     name=''
     okid=''
-    curl -k https://dnsapi.cn/Record.List -d "login_token=$login_token&domain_id=$domain_id" 2>/dev/null > ${TMPDIR}/get_record_id.xml
+    curl -k https://dnsapi.cn/Record.List -d "login_token=$login_token&domain_id=$domain_id&record_type=$record_type" 2>/dev/null > ${TMPDIR}/get_record_id.xml
     while read_xml_dom; do
         if [ "$ENTITY" = 'item' ]; then
             itemlevel=$DOMLVL
@@ -156,10 +177,11 @@ create_record()
     login_token=$1
     domain_id=$2
     record=$3
+    record_type=$4
 
     local DOMLVL=0 #初始化节点
 
-    curl -k https://dnsapi.cn/Record.Create -d "login_token=$login_token&domain_id=$domain_id&sub_domain=$record&record_type=A&record_line=默认&value=0.0.0.0" 2>/dev/null > ${TMPDIR}/create_record.xml
+    curl -k https://dnsapi.cn/Record.Create -d "login_token=$login_token&domain_id=$domain_id&sub_domain=$record&record_type=$record_type&record_line=默认&value=0.0.0.0" 2>/dev/null > ${TMPDIR}/create_record.xml
     while read_xml_dom; do
         if [ "$ENTITY" = 'id' ]; then
             id="$CONTENT"
@@ -212,6 +234,38 @@ ddns_record()
     fi
 }
 
+ddns_record_v6()
+{
+    login_token=$1
+    domain_id=$2
+    record_id=$3
+    record_v6=$4
+    ipv6=$5
+
+    local DOMLVL=0 #初始化节点
+
+    curl -k https://dnsapi.cn/Record.Modify -d "login_token=$login_token&domain_id=$domain_id&record_id=$record_id&sub_domain=$record_v6&record_type=AAAA&record_line=默认&value=$ipv6" 2>/dev/null > ${TMPDIR}/create_ddns.xml
+    while read_xml_dom; do
+        if [ "$ENTITY" = 'value' ]; then
+            value="$CONTENT"
+        fi
+        if [ "$ENTITY" = 'code' ]; then
+            code=$CONTENT
+        fi
+        if [ "$ENTITY" = 'message' ]; then
+            message="$CONTENT"
+        fi
+    done < ${TMPDIR}/create_ddns.xml
+
+    if [ "$code" = '1' ]; then
+        echo "$value";
+        return 0;
+    else
+        echo "$message";
+        return $code;
+    fi
+}
+
 init()
 {
     mkdir -p ${TMPDIR}
@@ -236,73 +290,145 @@ exiterr() {
 echo -n '初始化...'
 export TMPDIR='/tmp/ddns-dnspod'
 export OLDIPFILE="/tmp/ddns-dnspod-oldip"
+export OLDIPFILEV6="/tmp/ddns-dnspod-oldip-v6"
 init
 echo '[done]'
 
-echo -n '获取本地公网IP...'
-ip=$(get_localip) ||
-{
-    echo '[error]'
-    return -1
-} &&
-{
-    echo "[$ip]"
-}
-
-echo -n '比较上次IP...'
-oldip=$(cat "$OLDIPFILE" 2>/dev/null)
-if [ "$oldip" = "$ip" ]; then
-    echo '[nochange]'
-    exit 0
-else
-    echo "[change]"
-fi
-
 echo -n '读取配置文件...'
 . "$(dirname $0)/config.sh"
+if [ "$record_v6" = "" ]; then
+    record_v6="$record"
+fi
 echo '[done]'
 
-echo -n '获取domain_id...'
-return=$(get_domain_id "$login_token" "$domain") || 
-{
-    echo '[error]'
-    exiterr "$return"
-}
-domain_id=$return
-echo "[$domain_id]"
+v4() {
+    echo -n '获取本地公网IP...'
+    ip=$(get_localip) ||
+    {
+        echo '[error]'
+        exiterr "获取本地公网IP失败"
+    } &&
+    {
+        echo "[$ip]"
+    }
 
-echo -n '获取record_id...'
-return=$(get_record_id "$login_token" "$domain_id" "$record") || 
-{
-    echo '[error]'
-    exiterr "$return"
-}
-record_id=$return
-if [ "$record_id" = '' ]; then
-    echo '[null]'
+    echo -n '比较上次IP...'
+    oldip=$(cat "$OLDIPFILE" 2>/dev/null)
+    if [ "$oldip" = "$ip" ]; then
+        echo '[nochange]'
+        return 0
+    else
+        echo "[change]"
+    fi
 
-    echo -n '没有找到对应record_id，创建新record并获取id...'
-    return=$(create_record "$login_token" "$domain_id" "$record") || 
+    echo -n '获取domain_id...'
+    return=$(get_domain_id "$login_token" "$domain") || 
+    {
+        echo '[error]'
+        exiterr "$return"
+    }
+    domain_id=$return
+    echo "[$domain_id]"
+
+    echo -n '获取record_id...'
+    return=$(get_record_id "$login_token" "$domain_id" "$record" "A") || 
     {
         echo '[error]'
         exiterr "$return"
     }
     record_id=$return
-    echo "[$record_id]"
-else
-    echo "[$record_id]"
-fi
+    if [ "$record_id" = '' ]; then
+        echo '[null]'
 
-echo -n '更新DDNS...'
-return=$(ddns_record "$login_token" "$domain_id" "$record_id" "$record") || 
-{
-    echo '[error]'
-    exiterr "$return"
+        echo -n '没有找到对应record_id，创建新record并获取id...'
+        return=$(create_record "$login_token" "$domain_id" "$record" "A") || 
+        {
+            echo '[error]'
+            exiterr "$return"
+        }
+        record_id=$return
+        echo "[$record_id]"
+    else
+        echo "[$record_id]"
+    fi
+
+    echo -n '更新DDNS...'
+    return=$(ddns_record "$login_token" "$domain_id" "$record_id" "$record") || 
+    {
+        echo '[error]'
+        exiterr "$return"
+    }
+    value=$return
+    echo "[$value]"
+
+    echo "$ip" 2>/dev/null > $OLDIPFILE
 }
-value=$return
-echo "[$value]"
 
-echo "$ip" 2>/dev/null > $OLDIPFILE
+v6() {
+    echo -n '获取本地公网IPv6...'
+    ipv6=$(get_localip_v6) ||
+    {
+        echo '[error]'
+        exiterr "获取本地公网IPv6失败"
+    } &&
+    {
+        echo "[$ipv6]"
+    }
+
+    echo -n '比较上次IPv6...'
+    oldipv6=$(cat "$OLDIPFILEV6" 2>/dev/null)
+    if [ "$oldipv6" = "$ipv6" ]; then
+        echo '[nochange]'
+        return 0
+    else
+        echo "[change]"
+    fi
+
+    echo -n '获取domain_id...'
+    return=$(get_domain_id "$login_token" "$domain") || 
+    {
+        echo '[error]'
+        exiterr "$return"
+    }
+    domain_id=$return
+    echo "[$domain_id]"
+
+    echo -n '获取record_id...'
+    return=$(get_record_id "$login_token" "$domain_id" "$record_v6" "AAAA") || 
+    {
+        echo '[error]'
+        exiterr "$return"
+    }
+    record_id=$return
+    if [ "$record_id" = '' ]; then
+        echo '[null]'
+
+        echo -n '没有找到对应record_id，创建新record并获取id...'
+        return=$(create_record "$login_token" "$domain_id" "$record_v6" "AAAA") || 
+        {
+            echo '[error]'
+            exiterr "$return"
+        }
+        record_id=$return
+        echo "[$record_id]"
+    else
+        echo "[$record_id]"
+    fi
+
+    echo -n '更新DDNS...'
+    return=$(ddns_record_v6 "$login_token" "$domain_id" "$record_id" "$record_v6" "$ipv6") || 
+    {
+        echo '[error]'
+        exiterr "$return"
+    }
+    value=$return
+    echo "[$value]"
+
+    echo "$ipv6" 2>/dev/null > $OLDIPFILEV6
+}
+
+v4
+v6
 
 clean
 exit 0
